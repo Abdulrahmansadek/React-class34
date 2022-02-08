57ea9da0fd6f43a9749d76d639b569618bc45b71
Abdulrahmansadek <boody.sad@gmail.com>
2022-02-08 17:11:24 +0100

    fixing the categories data

--- week1/project/ecomerce/src/components/Header.jsx
+++ week1/project/ecomerce/src/components/Header.jsx
@@ -3,13 +3,14 @@ import React from "react";
 function Header({ onFilterCategories, category }) {
   const filterCategory = (e) => {
     onFilterCategories(e.target.value);
+    console.log(e.target.value);
   };
 
   return (
     <div onClick={filterCategory}>
       {category.map((item, idx) => {
         return (
-          <button key={idx} value={item}>
+          <button key={idx} value={item.slice(6)}>
             {item}
           </button>
         );
