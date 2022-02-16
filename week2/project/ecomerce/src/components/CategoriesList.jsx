abc77946ccf3080d65d27ead2aef5bcb95b4abbf
Abdulrahmansadek <boody.sad@gmail.com>
2022-02-16 12:02:08 +0100

    refactoring the ecomerce app

--- week2/project/ecomerce/src/components/CategoriesList.jsx
+++ week2/project/ecomerce/src/components/CategoriesList.jsx
@@ -1,74 +1,28 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React from "react";
+
 import Button from "./Button";
-import Error from "./Error";
-import IsLoading from "./IsLoading";
 
-function CategoriesList({ onFilterCategories, loading, setCategoriesLoading }) {
-  const [selected, setSelected] = useState();
-  const [categoryItem, setCategoryItem] = useState([]);
-  const [categoriesError, setCategoriesError] = useState(false);
-
-  useEffect(() => {
-    setCategoriesError(false);
-    setCategoriesLoading(true);
-    fetch("https://fakestoreapi.com/products/categories")
-      .then((res) => res.json())
-      .then((json) => {
-        setCategoriesLoading(false);
-        setCategoryItem(json);
-      })
-      .catch((error) => {
-        setCategoriesError(true);
-        console.log(error);
-        return <Error />;
-      });
-  }, []);
-
-  const filterCategory = (e) => {
-    onFilterCategories(e.target.value);
-    setSelected(e.target.value);
-  };
-  if (categoriesError) {
-    return <Error />;
-  } else if (loading) {
-    return (
-      <>
-        {loading && <IsLoading />}
-        <div className="categories-container" onClick={filterCategory}>
-          {categoryItem.map((item, idx) => {
-            return (
-              <Button
-                key={idx}
-                value={item}
-                style={selected === item ? "checked" : "category-btn"}
-              >
-                {item}
-              </Button>
-            );
-          })}
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className="categories-container" onClick={filterCategory}>
-          {categoryItem.map((item, idx) => {
-            return (
-              <Button
-                key={idx}
-                value={item}
-                style={selected === item ? "checked" : "category-btn"}
-              >
-                {item}
-              </Button>
-            );
-          })}
-        </div>
-      </>
-    );
-  }
-}
+const CategoriesList = ({
+  onFilterCategories,
+  categories,
+  selectedCategory,
+}) => {
+  return categories.map((item, idx) => (
+    <div
+      className="categories-container"
+      key={idx}
+      onClick={() => {
+        onFilterCategories(item);
+      }}
+    >
+      <Button
+        value={item}
+        style={selectedCategory === item ? "checked" : "category-btn"}
+      >
+        {item}
+      </Button>
+    </div>
+  ));
+};
 
 export default CategoriesList;
--- week2/project/ecomerce/src/components/ProductsList.jsx
+++ week2/project/ecomerce/src/components/ProductsList.jsx
@@ -4,62 +4,92 @@ import CategoriesList from "./CategoriesList";
 import Error from "./Error";
 import IsLoading from "./IsLoading";
 function ProductsList() {
-  const [productList, setProducts] = useState([]);
-  const [categories, setCategories] = useState("");
-  const [matchedCategoriesArray, setMatchedCategoriesArray] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [selectCategory, setSelectCategories] = useState("");
+  const [categories, setCategories] = useState([]);
+
   const [loading, setLoading] = useState(true);
   const [ErrorItem, setErrorItem] = useState(false);
   const [categoriesLoading, setCategoriesLoading] = useState(true);
 
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-  const fetchProducts = async () => {
+  const filterCategoriesHandler = (category) => {
+    if (selectCategory === category) {
+      setSelectCategories([]);
+      getProducts();
+
+      return;
+    } else {
+      setSelectCategories(category);
+    }
+    setLoading(true);
+  };
+
+  const getCategories = async () => {
+    try {
+      const res = await fetch("https://fakestoreapi.com/products/categories");
+      const data = await res.json();
+      setCategories(data);
+    } catch (error) {
+      setErrorItem(true);
+      setCategoriesLoading(false);
+    }
+  };
+
+  const getProducts = async () => {
     try {
       const res = await fetch("https://fakestoreapi.com/products");
       const data = await res.json();
-      setMatchedCategoriesArray(data);
       setProducts(data);
       setLoading(false);
+      setErrorItem(false);
     } catch (error) {
       setErrorItem(true);
+      setLoading(false);
     }
   };
 
-  const filterCategoriesHandler = (category) => {
-    if (categories === category) {
-      setMatchedCategoriesArray(productList);
-
-      return;
+  const getProductByCategories = async () => {
+    try {
+      const res = await fetch(
+        `https://fakestoreapi.com/products/category/${selectCategory}`
+      );
+      const data = await res.json();
+      setProducts(data);
+      setLoading(false);
+      setErrorItem(false);
+      setCategoriesLoading(true);
+    } catch (error) {
+      setErrorItem(true);
+      setCategoriesLoading(false);
     }
-    const filtered = productList.filter((catItem) => {
-      if (catItem.category === category) {
-        return catItem;
-      }
-    });
-    setCategoriesLoading(false);
-    setCategories(category);
-    setMatchedCategoriesArray(filtered);
   };
 
-  if (ErrorItem) {
-    return <Error />;
-  }
+  useEffect(() => {
+    getCategories();
+    getProducts();
+  }, []);
+  useEffect(() => {
+    getProductByCategories();
+  }, [selectCategory]);
 
   return (
     <>
-      {loading && <IsLoading />}
       <div className="categories-container">
         <CategoriesList
           onFilterCategories={filterCategoriesHandler}
-          loading={categoriesLoading}
-          setCategoriesLoading={setCategoriesLoading}
+          selectedCategory={selectCategory}
+          categories={categories}
         />
       </div>
       <div className="container">
-        {matchedCategoriesArray.map((item) => {
-          return <ProductsItem key={item.id} items={item} />;
-        })}
+        {loading === false ? (
+          products.map((item) => <ProductsItem key={item.id} items={item} />)
+        ) : (
+          <div>
+            {ErrorItem && <Error />}
+            {categoriesLoading && <IsLoading />}
+          </div>
+        )}
       </div>
     </>
   );
